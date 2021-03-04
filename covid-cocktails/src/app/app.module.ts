0298999054db02958f8a47de6a4351ebc3bad0ba
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-03-03 21:46:47 -0500

    display drink my drink name query

--- covid-cocktails/src/app/app.module.ts
+++ covid-cocktails/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SearchCriteriaComponent } from './search-criteria/search-criteria.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

667fbf02d4b4542bdd18889dc1bbba1a200b4e47
aspencer15 <amycspencer@gmail.com>
2021-03-04 19:50:23 -0500

    populate filter drodowns

--- covid-cocktails/src/app/cocktails.service.ts
+++ covid-cocktails/src/app/cocktails.service.ts
@@ -1,10 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailsService {
+
+  
   apiURL: string = 'https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=';
 
   constructor(private http: HttpClient) { }
@@ -12,5 +15,32 @@ export class CocktailsService {
   getCocktail(strDrink: string) {
     return this.http.get(`${this.apiURL}${strDrink}`)
   }
+
+  getIngredients() {
+    return this.http.get('https://www.thecocktaildb.com/api/json/v2/9973533/list.php?i=list');
+  }
+
+  getGlasses() {
+    return this.http.get('https://www.thecocktaildb.com/api/json/v2/9973533/list.php?g=list');
+  }
+
+  getAlcoholic() {
+    return this.http.get('https://www.thecocktaildb.com/api/json/v2/9973533/list.php?a=list');
+  } 
+
+  // getCocktailsByFilter(queryParams: any): Observable<any> {
+  //   let parameters: any = {};
+  //   if (queryParams.ingredient) {
+  //     parameters.ingredient= queryParams.value.ingredient;
+  //   }
+  //   if (queryParams.value.glass) {
+  //     parameters.glass = queryParams.value.glass;
+  //   }
+  //   if (queryParams.value.alcoholic) {
+  //     parameters.alcoholic = queryParams.value.alocohlic;
+  //   }
+  //   return this.http.get('https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=', {
+  //     params: parameters,
+  //   });
+  }
   
-}
--- covid-cocktails/src/app/search-criteria/search-criteria.component.ts
+++ covid-cocktails/src/app/search-criteria/search-criteria.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CocktailsService } from '../cocktails.service';
 import { Cocktail } from '../models/cocktail';
 
@@ -9,8 +12,19 @@ import { Cocktail } from '../models/cocktail';
 })
 export class SearchCriteriaComponent implements OnInit {
   cocktails: Cocktail[] = []
+  ingredient: any
+  glass: any;
+  alcoholic: any;
+  drinks: any;
+  ingredientSelected: any;
+  glassSelected: any;
+  alcoholicSelected: any;
+  
 
-  constructor(private cocktailsservice: CocktailsService) { }
+  constructor(
+    private cocktailsservice: CocktailsService,
+    private router: Router,
+    ) { }
 
   ngOnInit(): void {
     this.cocktailsservice.getCocktail('Gin').subscribe((data: any) => {
@@ -22,7 +36,41 @@ export class SearchCriteriaComponent implements OnInit {
           instructions: drink.strInstructions
         }
       });
-      console.log(this.cocktails);
+      // console.log(this.cocktails);
     })
+
+    this.cocktailsservice.getIngredients().subscribe((response: any) => {
+      this.ingredient = response['drinks'];
+      // console.log(this.ingredient);
+    });
+
+    this.cocktailsservice.getGlasses().subscribe((response: any) => {
+      this.glass = response['drinks'];
+    });
+
+    this.cocktailsservice.getAlcoholic().subscribe((response: any) => {
+      this.alcoholic = response['drinks'];
+    });
+    
   }
-  }
+
+  // findDrinksByFilter(form: NgForm) {
+  //   this.router.queryParams.subscribe((params) => {
+  //     console.log(params);
+  //   })
+  //   if (form.value.ingredient) {
+  //     parameters.ingredient= form.value.ingredient;
+  //   }
+  //   if (form.value.glass) {
+  //     parameters.glass = form.value.glass;
+  //   }
+  //   if (form.value.alcoholic) {
+  //     parameters.alcoholic = form.value.alocohlic;
+  //   }
+  //   this.cocktailsservice.getCocktailsByFilter().subscribe((response: any) => {
+  //     this.cocktails = response['cocktails'];
+  //     console.log(this.cocktails);
+  //   })
+  // }
+
+}

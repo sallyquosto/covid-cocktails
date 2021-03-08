4ea1995ca8b456720011bfc730f697cda9a76191
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-03-08 18:53:36 -0500

    details component fully functional for cocktail-list

--- covid-cocktails/src/app/search-criteria/search-criteria.component.ts
+++ covid-cocktails/src/app/search-criteria/search-criteria.component.ts
@@ -55,7 +55,23 @@ export class SearchCriteriaComponent implements OnInit {
         category: drink.strCategory,
         alcoholic: drink.strAlcoholic,
         instructions: drink.strInstructions,
-        image: drink.strDrinkThumb
+        image: drink.strDrinkThumb,
+        measure1: drink.strMeasure1,
+        measure2: drink.strMeasure2,
+        measure3: drink.strMeasure3,
+        measure4: drink.strMeasure4,
+        measure5: drink.strMeasure5,
+        measure6: drink.strMeasure6,
+        measure7: drink.strMeasure7,
+        measure8: drink.strMeasure8,
+        ingredient1: drink.strIngredient1,
+        ingredient2: drink.strIngredient2,
+        ingredient3: drink.strIngredient3,
+        ingredient4: drink.strIngredient4,
+        ingredient5: drink.strIngredient5,
+        ingredient6: drink.strIngredient6,
+        ingredient7: drink.strIngredient7,
+        ingredient8: drink.strIngredient8,
       }
     });
 }

4ac0dba95588d533a0b056d920596d1701395388
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-03-10 21:41:16 -0500

    added more properties to filter by alcoholic

--- covid-cocktails/src/app/search-criteria/search-criteria.component.ts
+++ covid-cocktails/src/app/search-criteria/search-criteria.component.ts
@@ -125,6 +125,13 @@ export class SearchCriteriaComponent implements OnInit {
             measure6: drink.strMeasure6,
             measure7: drink.strMeasure7,
             measure8: drink.strMeasure8,
+            measure9: drink.strMeasure9,
+            measure10: drink.strMeasure10,
+            measure11: drink.strMeasure11,
+            measure12: drink.strMeasure12,
+            measure13: drink.strMeasure13,
+            measure14: drink.strMeasure14,
+            measure15: drink.strMeasure15,
             ingredient1: drink.strIngredient1,
             ingredient2: drink.strIngredient2,
             ingredient3: drink.strIngredient3,
@@ -132,7 +139,14 @@ export class SearchCriteriaComponent implements OnInit {
             ingredient5: drink.strIngredient5,
             ingredient6: drink.strIngredient6,
             ingredient7: drink.strIngredient7,
-            ingredient8: drink.strIngredient8
+            ingredient8: drink.strIngredient8,
+            ingredient9: drink.strIngredient9,
+            ingredient10: drink.strIngredient10,
+            ingredient11: drink.strIngredient11,
+            ingredient12: drink.strIngredient12,
+            ingredient13: drink.strIngredient13,
+            ingredient14: drink.strIngredient14,
+            ingredient15: drink.strIngredient15,
 
           }
           this.cocktailsservice.cocktails.push(cocktail);
